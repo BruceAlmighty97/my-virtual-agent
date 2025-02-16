1ee1bcc3e0cb2a52a1c6b3924346be9a8a2eb741
Geoff Holland <geoff.dev.74@gmail.com>
2025-02-16 14:26:45 -0600

    add code to get call sid

--- telephony-service/controllers/socket.controller.ts
+++ telephony-service/controllers/socket.controller.ts
@@ -1,23 +1,28 @@
 import { IncomingMessage } from "http";
 import WebSocket, { WebSocketServer } from "ws";
+import { DeepgramService } from "../services/deepgram.service";
 
 export class SocketController {
     private _ws: WebSocketServer;
+    private _deepgramService: DeepgramService;
 
     constructor(ws: WebSocketServer) {
         this._ws = ws;
+        this._deepgramService = new DeepgramService();
     }
 
     public initialize() {
         this._ws.on("connection", (ws: WebSocket, request: IncomingMessage) => {
-            console.log("Connection established", request);
-
-            ws.on("message", (message) => {
-                console.log(`Received message => ${message}`);
-            });
+            const url = new URL(request.url || '', `wss://${request.headers.host}`);
+            const callSid = url.searchParams.get('CallSid');
+            console.log(`Call started for callSid ${callSid}`);
+            
+            // ws.on("message", (message) => {
+            //     console.log(`Received message => ${message}`);
+            // });
 
             ws.on("close", () => {
-                console.log("Connection closed");
+                console.log(`Call ended for callSid ${callSid}`);
             });
         });
     }
