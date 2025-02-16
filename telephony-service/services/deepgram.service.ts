dfff0a93cb9d937a51b0437ef0ac1b6a0d39f8ce
Geoff Holland <geoff.dev.74@gmail.com>
2025-02-16 14:20:24 -0600

    add code for listen socket

--- telephony-service/services/deepgram.service.ts
+++ telephony-service/services/deepgram.service.ts
@@ -1,3 +1,21 @@
+import { createClient, DeepgramClient } from "@deepgram/sdk";
+
 export class DeepgramService {
-    constructor() {}
+    private _client: DeepgramClient;
+
+    constructor() {
+        this._client = createClient(process.env.DEEPGRAM_API_KEY);
+    }
+
+    public getListenSocket() {
+        return this._client.listen.live({
+            model: "nova-2",
+            language: "multi",
+            encoding: "mulaw",
+            sample_rate: 8000,
+            interim_results: true,
+            endpointing: 500,
+            vad_events: true
+        });
+    }
 }
