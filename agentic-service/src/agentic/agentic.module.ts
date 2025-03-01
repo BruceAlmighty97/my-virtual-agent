302c3b4733389706ab8754c2f7e74a9b2477b7ac
Geoff Holland <geoff.dev.74@gmail.com>
2025-03-01 14:51:08 -0600

    add config and document service

--- agentic-service/src/agentic/agentic.module.ts
+++ agentic-service/src/agentic/agentic.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AgenticController } from './agentic.controller';
 import { DocumentService } from './document.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [AgenticController],
-  providers: [DocumentService]
+  providers: [DocumentService, ConfigService]
 })
 export class AgenticModule {}
--- agentic-service/src/app.module.ts
+++ agentic-service/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AgenticModule } from './agentic/agentic.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AgenticModule],
+  imports: [
+    AgenticModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [],
 })
