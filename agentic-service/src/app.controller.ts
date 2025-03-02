60f22da65e527a70516f3636bf1becc35da38e2d
Geoff Holland <geoff.dev.74@gmail.com>
2025-03-02 16:45:14 -0600

    test email functionality

--- agentic-service/src/app.controller.ts
+++ agentic-service/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 @Controller()
 export class AppController {
@@ -7,5 +8,24 @@ export class AppController {
   @Get()
   healthCheck(): string {
     return "ok";
+  }
+
+  @Get('/email')
+  async email(): Promise<string> {
+    const sesClient = new SESClient({ region: "us-east-1" });
+    const command:SendEmailCommand = new SendEmailCommand({
+      Source: 'geoff.dev.74@gmail.com',
+      Destination: {
+        ToAddresses: ['geoff.holland74@gmail.com']
+      },
+      Message: {
+        Subject: { Data: 'This is from my local app' },
+        Body: {
+          Text: {Data: 'Hello Geoffrey!!!!'}
+        }
+      }
+    });
+    const response = await sesClient.send(command);
+    return 'ok'
   }
 }
