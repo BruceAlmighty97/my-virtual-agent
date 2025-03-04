7ff91d217305e1ad84e966d8a969da95ddb63800
Geoff Holland <geoff.dev.74@gmail.com>
2025-03-04 13:59:15 -0600

    add start session

--- agentic-service/src/agentic/agentic.controller.ts
+++ agentic-service/src/agentic/agentic.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('agentic')
-export class AgenticController {}
+export class AgenticController {
+
+    constructor() {};
+
+    @Post('/start-session')
+    startSession(): string {
+        return "Session started";
+    }
+
+}
