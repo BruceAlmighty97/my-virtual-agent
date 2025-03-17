331dae38d15f6224e61e6178d554b8cba80762fc
Geoff Holland <geoff.dev.74@gmail.com>
2025-03-17 12:48:30 -0500

    remove old endpoint

--- agentic-service/src/agentic/agentic.controller.ts
+++ agentic-service/src/agentic/agentic.controller.ts
@@ -1,5 +1,4 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AgentGraphService } from './services/agent-graph.service';
 import { SimpleQueryRequestDto } from './dtos/simple-query-request.dto';
 import { SimpleQueryResponseDto } from './dtos/simple-query-response.dto';
 import { ApiKeyGuard } from './api-key.guard';
@@ -9,26 +8,12 @@ import { MyGraphService } from './services/my-graph.service';
 @UseGuards(ApiKeyGuard)
 export class AgenticController {
   constructor(
-    private _agentGraphService: AgentGraphService,
     private _myGraphService: MyGraphService
   ) {}
 
   @Post('/start-session')
   startSession(): string {
     return 'Session started';
-  }
-
-  @Post('/simple-query')
-  async simpleQuery(
-    @Body() request: SimpleQueryRequestDto,
-  ): Promise<SimpleQueryResponseDto> {
-    const answer = await this._agentGraphService.makeSimpleQuery(request);
-    // console.log(`Answer: ${answer}`);
-    return {
-      inputText: request.inputText,
-      outputText: answer,
-      sessionId: request.sessionId,
-    };
   }
 
   @Post('/call-my-graph')
